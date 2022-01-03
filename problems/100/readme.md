3b1b4cdb86466777b7f21e2d80efa17d5331bb62
behoney <bhoney1016@gmail.com>
2022-01-04 01:47:59 +0900

    2 files modified

--- problems/100/readme.md
+++ problems/100/readme.md
@@ -1,3 +1,14 @@
 # 100. Same Tree
 
 https://leetcode.com/problems/same-tree/
+
+- dfs first for each tree, (preorder)
+- the empty node filled with `-Infinity` because of following constraints
+```
+Constraints:
+
+The number of nodes in both trees is in the range [0, 100].
+-104 <= Node.val <= 104
+
+```
+
--- problems/100/solution.js
+++ problems/100/solution.js
@@ -1,10 +1,6 @@
 /**
  * Definition for a binary tree node.
- * function TreeNode(val, left, right) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.left = (left===undefined ? null : left)
- *     this.right = (right===undefined ? null : right)
- * }
+
  */
 /**
  * @param {TreeNode} p
@@ -12,5 +8,34 @@
  * @return {boolean}
  */
 var isSameTree = function (p, q) {
-  
+  const pTraversal = [];
+  const qTraversal = [];
+
+  const dfsPreorder = (node, traversal) => {
+    if (node == null) return;
+
+    traversal.push(node.val);
+    if (node.left != null) dfsPreorder(node.left, traversal);
+    else traversal.push(-Infinity)
+    if (node.right != null) dfsPreorder(node.right, traversal);
+    else traversal.push(-Infinity)
+  };
+  dfsPreorder(p, pTraversal);
+  dfsPreorder(q, qTraversal);
+
+  const isSameArr = (a, b) => {
+    let i = 0;
+    if (b.length != a.length) return false;
+    for (; i < a.length; i++) {
+      if (i >= b.length) return false;
+      if (a[i] != b[i]) return false;
+    }
+    return true;
+  };
+
+  const result = isSameArr(pTraversal, qTraversal);
+  console.log(pTraversal);
+  console.log(qTraversal);
+
+  return result;
 };

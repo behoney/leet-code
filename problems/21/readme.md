c663cf2c6e88c99b63c4be83d87211f22f8fa3ad
behoney <bhoney1016@gmail.com>
2022-01-17 11:24:08 +0900

    [add] solution for p21

---
+++ problems/21/readme.md
@@ -0,0 +1,5 @@
+# 21. Merge Two Sorted Lists
+
+
+
+https://leetcode.com/problems/max-area-of-island/
---
+++ problems/21/solution.ts
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function mergeTwoLists(
+  list1: ListNode | null,
+  list2: ListNode | null
+): ListNode | null {
+  const arr = [];
+  while (list1) {
+    arr.push(list1.val);
+    list1 = list1.next;
+  }
+  while (list2) {
+    arr.push(list2.val);
+    list2 = list2.next;
+  }
+  if (!arr.length) return null;
+  arr.sort((a, b) => a - b);
+  let head = new ListNode(arr.pop(), undefined);
+  while (arr.length) {
+    head = new ListNode(arr.pop(), head);
+  }
+  return head;
+}

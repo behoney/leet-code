8b6c53fe971699542970daf3b6165febcf16b684
behoney <bhoney1016@gmail.com>
2022-01-17 10:37:15 +0900

    [add] solution for p733

--- problems/733/solution.ts
+++ problems/733/solution.ts
@@ -4,59 +4,26 @@ function floodFill(
   sc: number,
   newColor: number
 ): number[][] {
-  const visited = [];
-  const result = [...image];
-  const width = image[0]?.length,
-    height = image?.length;
+  if (image.length == 1 && image[0].length == 1) return [[newColor]];
+  const maxR = image.length;
+  const maxC = image[0].length;
 
-  const targetColor: number = image[sr][sc];
+  let changed = false;
 
-  // const fill = (y, x) => {
-  //   result[y][x] = newColor;
-  //   checkTarget(y, x);
-  // };
-
-  const checkTarget = (sr, sc, pr = -1, pc = -1) => {
-    if(visited.indexOf([sr,sc]) != -1 ) return;
-    visited.push([sr,sc]);
-    console.log(sr, sc, pr, pc);
-    if (sr + 1 < height && result[sr + 1][sc] == targetColor) {
-      const y = sr + 1,
-        x = sc;
-      // fill(y, x);
-      console.log("test1", y, x);
-
-      result[y][x] = newColor;
-      if (pr != y || pc != x) checkTarget(y, x, sr, sc);
-    }
-    // 0                          1   0
-    if (sc + 1 < width && result[sr][sc + 1] == targetColor) {
-      const y = sr, // 1
-        x = sc + 1; // 1
-      // fill(y, x);
-      console.log("test2", y, x, sr, sc, pr, pc);
-      result[y][x] = newColor;
-      if (pr != y || pc != x) checkTarget(y, x, sr, sc);
-    }
-    if (sr - 1 >= 0 && result[sr - 1][sc] == targetColor) {
-      const y = sr - 1,
-        x = sc;
-      // fill(y, x);
-      console.log("test3", y, x);
-      result[y][x] = newColor;
-      if (pr != y || pc != x) checkTarget(y, x, sr, sc);
-    }
-    if (sc - 1 >= 0 && result[sr][sc - 1] == targetColor) {
-      const y = sr,
-        x = sc - 1;
-      // fill(y, x);
-      console.log("test4", y, x);
-      result[y][x] = newColor;
-      if (pr != y || pc != x) checkTarget(y, x, sr, sc);
-    }
+  const fillConnected = (r, c, targetColor) => {
+    console.log(r, c, image);
+    if (image[r][c] == newColor) return;
+    image[r][c] = newColor;
+    if (r + 1 < maxR && image[r + 1][c] == targetColor)
+      fillConnected(r + 1, c, targetColor);
+    if (c + 1 < maxC && image[r][c + 1] == targetColor)
+      fillConnected(r, c + 1, targetColor);
+    if (r - 1 >= 0 && image[r - 1][c] == targetColor)
+      fillConnected(r - 1, c, targetColor);
+    if (c - 1 >= 0 && image[r][c - 1] == targetColor)
+      fillConnected(r, c - 1, targetColor);
   };
-  result[sr][sc] = newColor;
-  checkTarget(sr, sc);
+  fillConnected(sr, sc, image[sr][sc]);
 
-  return result;
+  return image;
 }

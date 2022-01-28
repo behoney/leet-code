b2a066715babc5cb8e72b1859fdfbe5a2ea66608
behoney <bhoney1016@gmail.com>
2022-01-28 21:44:12 +0900

    [add] solving p15

--- problems/15/solution.ts
+++ problems/15/solution.ts
@@ -1,29 +1,48 @@
 function threeSum(nums: number[]): number[][] {
-  if (nums.length < 3) return [];
+  const numbers = [...nums].sort((a, b) => a - b);
+  const indexedResults = [];
 
-  
+  if (numbers.length < 3) return indexedResults;
 
-  const result = [];
-
-  for (let i = 0; i < nums.length; i++) {
-    for (let j = i + 1; j < nums.length; j++) {
-      for (let k = j + 1; k < nums.length; k++) {
-        if (nums[i] + nums[j] + nums[k] == 0)
-          result.push([nums[i], nums[j], nums[k]].sort((a, b) => a - b));
+  for (let i = 0; i < numbers.length; i++) {
+    for (let j = i + 1; j < numbers.length; j++) {
+      const target = numbers[i] + numbers[j];
+      const test = [...numbers].map((e) => e + target);
+      for (let k = j + 1; k < test.length; k++) {
+        if (test[k] == 0 && k != i && k != j) {
+          const sortedArr = [i, j, k];
+          sortedArr.sort((a, b) => a - b);
+          indexedResults.push(sortedArr);
+        }
       }
     }
   }
 
-  // console.log(result);
-  for (let i = 0; i < result.length; i++) {
-    for (let j = i + 1; j < result.length; j++) {
-      if (result[i].join() == result[j].join()) {
-        result.splice(j, 1);
-        // console.log(result);
-        j--;
+  const result = [...new Set(indexedResults.map((e) => e.join()))]
+    .map((e) => e.split(","))
+    .map((e) => {
+      const arr = [];
+      for (const v of e) {
+        arr.push(numbers[v]);
       }
-    }
-  }
+      return arr;
+    });
 
-  return result;
+  const removeDup = (arr: number[][]) => {
+    const test = [...arr].map((e) => e.sort()).map((e) => e.join());
+    const trav = [];
+    const IndexNeedToRemove = [];
+
+    for (let i = 0; i < test.length; i++) {
+      if (trav.indexOf(test[i]) == -1) trav.push(test[i]);
+      else IndexNeedToRemove.push(i);
+    }
+    while (IndexNeedToRemove.length > 0) {
+      arr[IndexNeedToRemove.pop()] = [];
+    }
+  };
+
+  removeDup(result);
+
+  return result.filter((e) => e.length == 3);
 }

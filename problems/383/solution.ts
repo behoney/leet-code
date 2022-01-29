549221e238751e8a38a261969c9f193c743700da
behoney <bhoney1016@gmail.com>
2022-01-29 14:41:33 +0900

    [update] solution for p383

--- problems/383/solution.ts
+++ problems/383/solution.ts
@@ -1,10 +1,10 @@
 function canConstruct(ransomNote: string, magazine: string): boolean {
-  const set = magazine.split("");
-  const target = ransomNote.split("");
+  const set = magazine.split("").sort();
+  const target = ransomNote.split("").sort();
 
   for (const e of set) {
-
     if (target.indexOf(e) != -1) target[target.indexOf(e)] = "-";
+    if (target.every((e) => e == "-")) return true;
   }
 
   return target.every((e) => e == "-");

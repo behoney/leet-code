51fcef9dbba3d7a84df1678a69f59e6e2dff880e
behoney <bhoney1016@gmail.com>
2022-01-29 15:05:22 +0900

    [add] solution for p986

--- problems/986/solution.ts
+++ problems/986/solution.ts
@@ -1,3 +1,14 @@
-function intervalIntersection(firstList: number[][], secondList: number[][]): number[][] {
+function intervalIntersection(
+  firstList: number[][],
+  secondList: number[][]
+): number[][] {
+  const result = [];
+  for (const a of firstList) {
+    for (const b of secondList) {
+      if (a[0] > b[1] || b[0] > a[1]) continue;
+      result.push([Math.max(a[0], b[0]), Math.min(a[1], b[1])]);
+    }
+  }
 
-};
+  return result;
+}

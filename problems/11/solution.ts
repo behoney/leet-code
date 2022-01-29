7ac7e7d564ef15849bf6fff85f969de360a7aa76
behoney <bhoney1016@gmail.com>
2022-01-29 14:56:04 +0900

    [add] solving p11

--- problems/11/solution.ts
+++ problems/11/solution.ts
@@ -1,3 +1,12 @@
 function maxArea(height: number[]): number {
+  let result = -Infinity;
 
-};
+  for (let i = 0; i < height.length; i++) {
+    for (let j = i + 1; j < height.length; j++) {
+      const area = (j - i) * Math.min(height[i], height[j]);
+      result = Math.max(result, area);
+    }
+  }
+
+  return result;
+}

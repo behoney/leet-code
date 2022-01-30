338199d1e32ce87fb20595274b25f64ce1af14f5
BhoneyKim <bhoney1016@gmail.com>
2022-01-31 01:31:50 +0900

    [add] solution for p209

--- problems/209/solution.ts
+++ problems/209/solution.ts
@@ -1,4 +1,37 @@
 function minSubArrayLen(target: number, nums: number[]): number {
+  let i = 0;
+  let j = 0;
+  let sum = undefined;
+  let min = Infinity;
+
+  const len = nums.length;
+
+  while (i < len && j < len) {
+
+    // the minimum can't be less that one ;)
+    if (min === 1 || nums[i] >= target) return 1;
+
+    if (sum === undefined) {
+      // the first number
+      sum = nums[i];
+      i++;
+    }
+
+    while (sum < target) {
+      sum += nums[i];
+      i++;
+    }
+
+    while (sum >= target) {
+      min = Math.min(min, i - j);
+      sum -= nums[j];
+      j++;
+    }
+  }
+
+  return min === Infinity ? 0 : min;
+//----------------------------------------------------------------
+
   if (nums.some(e => e == target)) return 1;
   if (nums.length == 1) return nums[0] == target ? 1 : 0
 

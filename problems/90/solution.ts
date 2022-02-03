9339ecbe0a1c15b07fe9d2057ad5e114bb52d19b
behoney <bhoney1016@gmail.com>
2022-02-03 23:07:00 +0900

    [add] solution for p90

--- problems/90/solution.ts
+++ problems/90/solution.ts
@@ -9,6 +9,7 @@ function subsetsWithDup(nums: number[]): number[][] {
 
   const subsetsByIndex = (cur: number[]) => {
     if (cur.length == 0) return;
+    // console.log(cur)
     set.add(JSON.stringify(cur));
     if (cur.length == 1) return;
     for (let i = 0; i < cur.length; i++) {
@@ -18,14 +19,37 @@ function subsetsWithDup(nums: number[]): number[][] {
     }
   }
 
-  subsetsByIndex(nums);
+  subsetsByIndex(indexes);
 
+  // console.log("set", set)
 
   // parse again
+  const result: number[][] = [...set].map((e: string) => {
+    const indexedArr: number[] = JSON.parse(e)
+    const targetArr: number[] = []
+    // console.log("indexedArr", indexedArr);
+
+    for (const t of indexedArr)
+      targetArr.push(nums[t]);
+
+    return targetArr.sort();
+  });
+  // console.log("result", result);
+
 
   //translate index to values from nums
+  result.push([])
 
-  return [[]];
+  //remove again
+  const removeDuplicates = (arr: number[][]): number[][] => {
+    const set = new Set<string>()
+    for (const e of arr) set.add(JSON.stringify(e))
+    const result = [...set].map((e: string) => {
+      return JSON.parse(e);
+    })
 
+    return result;
+  }
 
+  return removeDuplicates(result);
 };

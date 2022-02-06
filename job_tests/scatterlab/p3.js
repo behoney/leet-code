2eb191ce376f836770c75bcc310a8bcdebd69f06
behoney <bhoney1016@gmail.com>
2022-02-06 20:54:02 +0900

    [add] methods that check `primes` changed

--- job_tests/scatterlab/p3.js
+++ job_tests/scatterlab/p3.js
@@ -1,42 +1,45 @@
-const isPrimes = (n) => {
-    let val = n - 1;
-    while (val > 1) {
-        // if (n % val == 0) return false;
-        const dividedResult = n / val;
-        if (dividedResult == Math.floor(n / val)) return false;
-        val--;
-    }
-    return true;
+// const isPrimes = (n) => {
+//   let val = n - 1;
+//   while (val > 1) {
+//     // if (n % val == 0) return false;
+//     const dividedResult = n / val;
+//     if (dividedResult == Math.floor(dividedResult)) return false;
+//     val--;
+//   }
+//   return true;
+// };
+
+const isPrimes = (num) => {
+    for (let i = 2, s = Math.sqrt(num); i <= s; i++)
+        if (num % i === 0) return false;
+    return num > 1;
 };
 
 const addAll = (acc, cur) => acc + cur;
+const primes = [];
 
 function solution(N, M) {
     // collect primes
-    const primes = [];
     for (let i = N; i > 1; i--)
         if (i <= M && isPrimes(i)) primes.push(i);
 
         //   const resultArr = [];
     let result = 0;
 
-    const getCombination = (idx, current = []) => {
-        const acc = current.reduce(addAll, 0);
-
-        if (acc == M) {
+    const getCombination = (idx, acc) => {
+        if (acc >= M) {
             //   resultArr.push(current);
-            result++;
+            result += acc == M ? 1 : 0;
             return;
         }
-        if (acc > M) return;
 
         // getCombination(idx + 1, [...current, primes[idx + 1]]);
-        if (idx + 1 < primes.length)
-            getCombination(idx + 1, [acc, primes[idx + 1]]);
+        if (idx + 1 >= primes.length) return;
+        getCombination(idx + 1, acc + primes[idx + 1]);
     };
 
     for (let i = 0; i < primes.length; i++) {
-        getCombination(i, [primes[i]]);
+        getCombination(i, primes[i]);
     }
 
     return result;

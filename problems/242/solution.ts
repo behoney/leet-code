886f9ae9580aae079dcba540b3c28dfe9de7e112
behoney <bhoney1016@gmail.com>
2022-02-12 17:28:08 +0900

    [add] new solution for p242

--- problems/242/solution.ts
+++ problems/242/solution.ts
@@ -1,3 +1,21 @@
 function isAnagram(s: string, t: string): boolean {
-  return s.split("").sort().join() == t.split("").sort().join();
+  if (s.length != t.length) return false;
+
+  const obj = {}
+  for (let i = 0; i < s.length; i++) {
+    if (obj[s[i]] == undefined)
+      obj[s[i]] = 1
+    else
+      obj[s[i]] += 1
+  }
+  for (let i = 0; i < t.length; i++) {
+    if (obj[t[i]] == undefined)
+      return false;
+    else {
+      obj[t[i]] -= 1;
+      if (obj[t[i]] == 0) delete obj[t[i]]
+    }
+  }
+  return true;
+
 }

c8f4d8161c047f4d6934cc4e847f32911132d494
behoney <bhoney1016@gmail.com>
2022-02-25 07:14:54 +0000

    [add] solving p746 and dp concept

--- problems/746/readme.md
+++ problems/746/readme.md
@@ -2,3 +2,6 @@
 
 
 https://leetcode.com/problems/min-cost-climbing-stairs/
+
+
+need to check : https://leetcode.com/problems/min-cost-climbing-stairs/discuss/476388/4-ways-or-Step-by-step-from-Recursion-greater-top-down-DP-greater-bottom-up-DP-greater-fine-tuning
--- problems/746/solution.ts
+++ problems/746/solution.ts
@@ -2,14 +2,23 @@ function minCostClimbingStairs(cost: number[]): number {
   if (cost.length <= 2) return Math.min(...cost)
 
   // 2 2 1 0
+  // mincost(i) = Math.min(mincost(i-1), mincost(i-2))
+
+  const dp = new Array(cost.length).fill(0);
 
   const getMinCost = (idx) => {
-    if (idx >= cost.length - 2) return cost[idx];
+    if (idx < 0) return 0;
+    if (idx == 0 || idx == 1) return cost[idx];
 
-    return cost[idx] + Math.min(
-      getMinCost(idx + 1),
-      getMinCost(idx + 2));
+    if (dp[idx] != 0) return dp[idx];
+
+    dp[idx] = cost[idx] + Math.min(
+      getMinCost(idx - 1),
+      getMinCost(idx - 2)
+    );
+
+    return dp[idx]
   }
 
-  return Math.min(getMinCost(0), getMinCost(1))
+  return Math.min(getMinCost(cost.length - 1), getMinCost(cost.length - 2))
 };

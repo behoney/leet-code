e75261f65d937f3d75b2dc1d7b725de9bdb564e7
behoney <bhoney1016@gmail.com>
2022-02-27 13:49:25 +0000

    [update] useless log

--- problems/740/solution.ts
+++ problems/740/solution.ts
@@ -6,21 +6,23 @@ function deleteAndEarn(nums: number[]): number {
     if (cur.length === 0) return 0;
 
     const target = cur[idx];
+    cur[idx] += 1;          // will be removed by following filter
 
-    cur.splice(idx, 1);
     const rem
       = cur.filter(
-        e => { return e != target + 1 && e != target - 1 }
+        e => { return e == target || e < target - 1 || e > target + 1 }
       )
+    if (rem.length === 0) return target;
 
     const key = rem.join('');
 
     // console.log("target", key, rem);
     if (dp[key] === undefined) {
-      if (rem.length === 0) return target;
       dp[key] = getMax([...rem], 0);
+
       for (let i = 1; i < rem.length; i++)
         dp[key] = Math.max(dp[key], getMax([...rem], i))
+
       return target + dp[key];
     }
 
@@ -28,6 +30,7 @@ function deleteAndEarn(nums: number[]): number {
   }
 
   let result = -Infinity;
+
   for (let idx = 0; idx < nums.length; idx++) {
     result = Math.max(result, getMax([...nums], idx))
   }

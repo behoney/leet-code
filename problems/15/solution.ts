9398381cc669d48bf7ff060057db44459c1f0ce3
behoney <bhoney1016@gmail.com>
2022-03-03 07:14:27 +0000

    [add] solving p15 / reduce entries by bounding

--- problems/15/solution.ts
+++ problems/15/solution.ts
@@ -1,29 +1,35 @@
 function threeSum(nums: number[]): number[][] {
   if (nums.length < 3) return [];
-
   nums.sort((a, b) => a - b)
+
+  const max = nums[nums.length - 1];
+  const min = nums[0];
+
   const result = new Set<string>();
   const obj = {
   }
   for (let i = 0; i < nums.length; i++) {
     for (let j = i + 1; j < nums.length; j++) {
-      if (obj[-(nums[i] + nums[j])] == undefined)
-        obj[-(nums[i] + nums[j])] = [[i, j]]
+      const target = -(nums[i] + nums[j]);
+
+      if (target > max || target < min) continue;
+
+      if (obj[target] == undefined)
+        obj[target] = [[i, j]]
       else {
-        obj[-(nums[i] + nums[j])].push([i, j])
+        obj[target].push([i, j])
       }
     }
   }
-  // console.log(obj);
+
+  console.log(obj);
 
   for (let i = 0; i < nums.length; i++) {
     if (obj[nums[i]] != undefined) {
       obj[nums[i]].map(e => {
         if (!e.includes(i)) {
-
           // console.log([...e, i]);
-
-          result.add(JSON.stringify([...e.map(v => nums[v]), nums[i]].sort()))
+          result.add(JSON.stringify([nums[e[0]], nums[e[1]], nums[i]].sort()))
         }
       })
     }

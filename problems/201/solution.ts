d5a8b6fe1f21fd5bb1566aba76f55e40053e9250
behoney <bhoney1016@gmail.com>
2022-03-06 05:57:51 +0000

    [add] solution for p201

--- problems/201/solution.ts
+++ problems/201/solution.ts
@@ -1,16 +1,13 @@
-function rangeBitwiseAnd(left: number, right: number): number {
+function rangeBitwiseAnd(left, right) {
   if (left == 0 || right == 0) return 0;
   if (left == right) return right;
 
-  const buf = right.toString(2).split('').reverse();
+  let result = right;
   for (let i = left; i <= right; i++) {
-    const operand = i.toString(2).split('').reverse();
-    while (operand.length < buf.length) operand.push('0');
-    for (let j = 0; j < operand.length; j++) {
-      if (buf[j] == "0" || operand[j] == "0") buf[j] = '0'
-    }
-
-    if (buf.every(e => e == '0')) return 0;
+    // console.log(result.toString(2), i.toString(2))
+    result = result & i
+    if(result == 0) break;
   }
-  return parseInt(buf.reverse().join(''), 2);
+
+  return result;
 };

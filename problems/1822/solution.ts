898a68ec590caeed79a252d4de4aa265cd5e43d2
behoney <bhoney1016@gmail.com>
2022-03-16 05:29:44 +0000

    [update] solution for p1822

--- problems/1822/solution.ts
+++ problems/1822/solution.ts
@@ -1,3 +1,12 @@
 function arraySign(nums: number[]): number {
+  if (nums.includes(0)) return 0;
 
+  const result: number[] = nums.map(e => {
+    if (e > 0) return 1;
+    else return -1;
+  });
+
+  return result.reduce((acc, e) => {
+    return acc * e
+  }, 1);
 };

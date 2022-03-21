00e11d038c742124fbe7ca70295ffcb496eabf45
behoney <bhoney1016@gmail.com>
2022-03-21 04:50:37 +0000

    [add] solution for p1290

---
+++ problems/1290/solution.ts
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function getDecimalValue(head: ListNode | null): number {
+  let str = "";
+  let ptr = head;
+  while (ptr != null) {
+    str += ptr.val.toString();
+    ptr = ptr.next;
+  }
+  return parseInt(str, 2);
+};

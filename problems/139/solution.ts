3b79a01b5b9e675a9e84b4b27af6958191ce1a9f
behoney <bhoney1016@gmail.com>
2022-04-05 06:46:17 +0000

    [add] solution for p139

--- problems/139/solution.ts
+++ problems/139/solution.ts
@@ -10,31 +10,44 @@ function wordBreak(s: string, wordDict: string[]): boolean {
     segmentChecker = segmentChecker || s.includes(wordDict[i]);
   }
   if (!segmentChecker) return false;
-  for (let i = 0; (i < wordDict.length); i++) {
-    if (wordDict[i].length === 2) {
 
-      const target = wordDict[i][1] + wordDict[i][0];
-      if (wordDict.includes(target)) {
-        wordDict[i] = target[0];
-        wordDict[wordDict.indexOf(target)] = target[1];
-      }
+  wordDict.sort();
+
+  let result = new Array(s.length).fill(false);
+  result.push(true);
+
+  for (let i = s.length; i >= 0; i--) {
+    for (const e of wordDict) {
+      if (s.length - i >= e.length && e === s.substring(i, i + e.length))
+        result[i] = result[i + e.length];
+      if (result[i]) break;
     }
   }
 
-  const testset = wordDict.sort().reverse();
+  // for (let i = 0; (i < wordDict.length); i++) {
+  //   if (wordDict[i].length === 2) {
+  //     const target = wordDict[i][1] + wordDict[i][0];
+  //     if (wordDict.includes(target)) {
+  //       wordDict[i] = target[0];
+  //       wordDict[wordDict.indexOf(target)] = target[1];
+  //     }
+  //   }
+  // }
 
-  for (let i = 0; i < testset.length; i++) {
-    let str = s;
-    for (let j = 0; j < testset.length; j++) {
-      const target = testset[(i + j) % testset.length];
-      // console.log(str, "<", target);
-      while (str.includes(target))
-        str = str.replace(target, target.toUpperCase())
-    }
-    // console.log(str);
+  // const testset = wordDict.sort().reverse();
 
-    if (str === str.toUpperCase()) return true;
-  }
+  // for (let i = 0; i < testset.length; i++) {
+  //   let str = s;
+  //   for (let j = 0; j < testset.length; j++) {
+  //     const target = testset[(i + j) % testset.length];
+  //     // console.log(str, "<", target);
+  //     while (str.includes(target))
+  //       str = str.replace(target, target.toUpperCase())
+  //   }
+  //   // console.log(str);
 
-  return false;
+  //   if (str === str.toUpperCase()) return true;
+  // }
+
+  return result[0];
 };

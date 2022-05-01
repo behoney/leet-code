ab12be8496ab3c1de450a05ebd0f0dfe206ce3d5
behoney <bhoney1016@gmail.com>
2022-05-01 01:33:53 +0000

    [add] rev-solution for p844

--- problems/844/solution.ts
+++ problems/844/solution.ts
@@ -1,14 +1,15 @@
+const resultStr = (s) => {
+  const arr = s.split("");
+
+  while (arr.some((e) => e === "#")) {
+    if (arr.indexOf("#") === 0) arr.splice(arr.indexOf("#"), 1);
+    else arr.splice(arr.indexOf("#") - 1, 2);
+  }
+
+  return arr.join("");
+};
+
 function backspaceCompare(s: string, t: string): boolean {
-  const resultStr = (s) => {
-    const arr = s.split("");
 
-    while (arr.some((e) => e == "#")) {
-      if (arr.indexOf("#") == 0) arr.splice(arr.indexOf("#"), 1);
-      else arr.splice(arr.indexOf("#") - 1, 2);
-    }
-
-    return arr.join("");
-  };
-
-  return resultStr(s) == resultStr(t);
+  return resultStr(s) === resultStr(t);
 }

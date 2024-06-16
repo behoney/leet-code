a50c72b82160d72942a5b49319727f8edcdc546e
Byounghern Kim <bhoney1016@gmail.com>
2024-06-16 12:52:07 +0900

    chore: p104 rep 2

--- problems/104/solution.js
+++ problems/104/solution.js
@@ -16,3 +16,29 @@ var maxDepth = function (root, depth = 0) {
   depth++;
   return Math.max(maxDepth(root.left, depth), maxDepth(root.right, depth), depth);
 };
+
+
+// rep 2
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function(root) {
+    return helper(root)
+    
+};
+
+const helper = (node)=>{
+    if(!node) return 0
+    if(node.left === null && node.right === null) return 1
+    return 1 + Math.max (helper(node.left), helper(node.right))
+}

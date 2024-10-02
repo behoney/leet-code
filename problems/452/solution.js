301d1f59727d1a37ecf25d42b3157d825c732dc9
Byounghern Kim <bhoney1016@gmail.com>
2024-10-02 20:47:34 +0900

    chore: rep2 on p452

--- problems/452/solution.js
+++ problems/452/solution.js
@@ -1,5 +1,31 @@
+// NOTE:: rep2 ~ 2min 30sec
+/**
+ * @param {number[][]} points
+ * @return {number}
+ */
+var findMinArrowShots = function(points) {
+    points.sort((a,b)=>a[0]-b[0])
+
+    // [ [ 1, 6 ], [ 2, 8 ], [ 7, 12 ], [ 10, 16 ] ]
+    const merged = []
+    merged.push(points.at(0))
+
+    for(let i=1; i<points.length; i++){
+
+        if(merged.at(-1)[1] >= points.at(i)[0] )
+        {
+            merged.at(-1)[0] = Math.max(merged.at(-1)[0], points.at(i)[0])
+            merged.at(-1)[1] = Math.min(merged.at(-1)[1], points.at(i)[1])
+        }
+        else merged.push([...points.at(i)])
+    }
+
+    // console.log(merged)
+
+    return merged.length
+};
+
 // NOTE:: rep1 ~ 30min 49sec
-// NOTE:: rep2 required
 /**
  * @param {number[][]} points
  * @return {number}
